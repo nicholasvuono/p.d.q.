178bf31219330d342b4d099f849ce670c5287972
nvuono <nvuono@outlook.com>
2020-10-23 02:14:29 -0400

    Added TTFB & TBT
    added two more load tiem metrics: time to first byte and total blocking time

--- page-load.go
+++ page-load.go
@@ -24,18 +24,39 @@ func getTimeToInteractive(page *rod.Page) string {
 	return strconv.FormatUint(timeToInteractive, 10)
 }
 
+func getTotalBlockingTime(firstContentfulPaint string, timeToInteractive string) string {
+	fcp, err := strconv.Atoi(firstContentfulPaint)
+	explain(err)
+	tti, err := strconv.Atoi(timeToInteractive)
+	explain(err)
+	return strconv.Itoa(tti - fcp)
+}
+
+func getTimeToFirstByte(page *rod.Page) string {
+	metrics, err := page.Eval("JSON.stringify(performance.toJSON())")
+	explain(err)
+	requestStart := gjson.Get(metrics.Value.String(), "timing.requestStart").Uint()
+	responseStart := gjson.Get(metrics.Value.String(), "timing.responseStart").Uint()
+	timeToFirstByte := responseStart - requestStart
+	return strconv.FormatUint(timeToFirstByte, 10)
+}
+
 func getPageLoadTimings(page *rod.Page) string {
 	fcp := getFirstContentfulPaint(page)
 	tti := getTimeToInteractive(page)
+	tbt := getTotalBlockingTime(fcp, tti)
+	ttfb := getTimeToFirstByte(page)
 	pageLoadTimings := fmt.Sprintf(`
 	First Contentful Paint (FCP):		%vms
 	Time to Interactive (TTI):			%vms
-	`, fcp, tti)
+	Total Blocking Time (TBT):		%vms
+	Time to First Byte (TTFB):		%vms
+	`, fcp, tti, tbt, ttfb)
 	return pageLoadTimings
 }
 
 func getPageLoadTimingsOverlay(page *rod.Page) {
 	pageLoadTimings := getPageLoadTimings(page)
-	inlineJS := `(pageLoadTimings) => {let existing = document.getElementById('PageLoadTimings');if(existing === null) {let body = document.querySelector('body');let div = document.createElement('div');div.setAttribute('id', 'PageLoadTimings');div.setAttribute('style', 'font-family:Segoe UI;color:#00FFFF;position:fixed;background:rgba(0,0,0,.8);z-index:99000000000;left:0px;bottom:0px;width:280px;height:60px;pointer-events:none;font-size:small;white-space:pre-wrap;');div.textContent = pageLoadTimings;body.appendChild(div);} else {existing.textContent = pageLoadTimings;}}`
+	inlineJS := `(pageLoadTimings) => {let existing = document.getElementById('PageLoadTimings');if(existing === null) {let body = document.querySelector('body');let div = document.createElement('div');div.setAttribute('id', 'PageLoadTimings');div.setAttribute('style', 'font-family:Segoe UI;color:#00FFFF;position:fixed;background:rgba(0,0,0,.8);z-index:99000000000;left:0px;bottom:0px;width:300px;height:100px;pointer-events:none;font-size:small;white-space:pre-wrap;');div.textContent = pageLoadTimings;body.appendChild(div);} else {existing.textContent = pageLoadTimings;}}`
 	page.MustEval(inlineJS, pageLoadTimings)
 }
